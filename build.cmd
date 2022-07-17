942e3a77995c93fcb42e7585382483e0b7d2a149
Tanguy Baltazart <tbalta914@gmail.com>
2022-07-17 15:32:36 +0200

    build build -> publish

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-dotnet build .\src\
+dotnet publish .\src\
 copy src\bin\Debug\netcoreapp3.1\publish\Azurite.dll %USERPROFILE%\.azurite /y
 copy src\bin\Debug\netcoreapp3.1\publish\Azurite.exe %USERPROFILE%\.azurite /y
 copy src\bin\Debug\netcoreapp3.1\publish\Azurite.pdb %USERPROFILE%\.azurite /y
